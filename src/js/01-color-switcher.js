9f464149d313361d7c21aeb665a66c010759af6f
Andrii Chertkov <andriichertkov@gmail.com>
2022-11-13 13:46:05 +0200

    Task-1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,37 @@
+const refs = {
+  startBtn: document.querySelector('[data-start]'),
+  stopBtn: document.querySelector('[data-stop]'),
+  body: document.querySelector('body'),
+};
 
+refs.startBtn.addEventListener('click', onStartchangeColor);
+refs.stopBtn.addEventListener('click', onStopChangeColor);
+
+let idInterval = null;
+
+function onStartchangeColor(e) {
+  idInterval = setInterval(() => {
+    refs.body.style.backgroundColor = randomColor();
+  }, 1000);
+
+  onStartAttribute();
+}
+
+function onStopChangeColor(e) {
+  clearInterval(idInterval);
+  onStopAttribute();
+}
+
+const randomColor = function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
+
+function onStopAttribute() {
+  refs.stopBtn.setAttribute('disabled', true);
+  refs.startBtn.removeAttribute('disabled');
+}
+
+function onStartAttribute() {
+  refs.startBtn.setAttribute('disabled', true);
+  refs.stopBtn.removeAttribute('disabled');
+}
